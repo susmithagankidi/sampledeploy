adbcb50505f6f3e24ee614a91f60f45d09767a05
susmithagankidi <gankidisusmitha@gmail.com>
2020-09-06 22:49:34 -0400

    Delete testdocker.sh

--- scripts/testdocker.sh
+++
@@ -1,3 +0,0 @@
-#!bin/bash
-aws ecr get-login-password  --region ap-south-1 | docker login  --username AWS  --password-stdin 272842187865.dkr.ecr.ap-south-1.amazonaws.com
-docker pull 272842187865.dkr.ecr.ap-south-1.amazonaws.com/testbuild:customtest
